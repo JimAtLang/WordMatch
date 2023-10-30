87045323f47a93ab3ccb2b0b54eaf93df3cd998d
jimatlang <jim.stewart@thelangschool.org>
2023-10-30 10:20:22 -0400

    ScoreGuess implemented

--- Main.java
+++ Main.java
@@ -1,4 +1,13 @@
 public class Main {
-    WordMatch wm = new WordMatch("Mississippi");
-    UI ui = new UI();
+    WordMatch wm = new WordMatch("mississippi");
+    Ui ui = new Ui();
+    public static void main(String[] args){
+        new Main();
+    }
+    public Main(){
+        String guess = ui.inputString("what is your guess?");
+        int score = wm.ScoreGuess(guess);
+        System.out.println("Your score is " + score);
+
+    }
 }
--- WordMatch.java
+++ WordMatch.java
@@ -5,6 +5,24 @@ public class WordMatch {
         this.secret = secret;
     }
     public int ScoreGuess(String guess){
-        return 0;
+        int slen = secret.length();
+        int glen = guess.length();
+        if (glen>slen){
+            return 0;
+        }
+        int freq = 0;
+        for(int i=0;i<slen-glen+1;i++){
+            boolean match = true;
+            for(int j=0;j < glen; j++){
+                if(secret.charAt(i+j)!=guess.charAt(j)){
+                    match = false;
+                    break;
+                }
+            }
+            if(match){
+                freq++;
+            }
+        }
+        return freq * glen * glen;
     }
 }

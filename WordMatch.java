3be7170ae742a29cd2d15403150495b56a388b4c
US Breakout <us.breakout@thelangschool.net>
2023-12-11 14:02:38 -0500

    began implementing algorithm

--- WordMatch.java
+++ WordMatch.java
@@ -1,10 +1,16 @@
 public class WordMatch {
-    private String secret;
+    private String secret; //this is the state!!
 
     public WordMatch(String secret) {
         this.secret = secret;
     }
+    //FOR NOW, return 1 if guess is in secret, and -1 if it's not
     public int ScoreGuess(String guess){
+        for(int i=0; i < secret.length(); i++){
+            char s = secret.charAt(i);
+            char g = guess.charAt(i);
+            
+        }
         return 0;
     }
 }
